020af1897b75103314fafee19052acf6ada4ad74
Luis Kamt <luis.kamt@utec.edu.pe>
2026-06-29 23:00:29 -0500

    Update 1

--- ar_ws/src/nodes/setup.py
+++ ar_ws/src/nodes/setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'VoiceInteractionNode = nodes.VoiceInteractionNode:main',
+            'CommandInterpreter = nodes.CommandInterpreter:main'
         ],
     },
 )
